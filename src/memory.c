470e38c7276ca057a783db48d9621773c944ccd6
Michael Cordover <khefin@mjec.net>
2021-01-17 12:05:24 -0500

    Stop dropping privileges if run under sudo (closes #35)

--- src/memory.c
+++ src/memory.c
@@ -13,7 +13,6 @@
 
 void lock_memory_and_drop_privileges(void) {
 	int r;
-	int original_uid;
 
 	// Prevent our memory from being swapped out
 	r = mlockall(MCL_CURRENT | MCL_FUTURE);
@@ -47,39 +46,6 @@ void lock_memory_and_drop_privileges(void) {
 #endif
 
 #endif // ifndef DEBUG
-
-	// Drop privileges if running as root
-	if (geteuid() == 0) {
-		if (geteuid() != getuid()) {
-			original_uid = getuid();
-		} else if (getenv("SUDO_UID") != NULL) {
-			original_uid = atoi(getenv("SUDO_UID"));
-			if (original_uid < 1) {
-				original_uid = 0;
-			}
-		} else {
-			original_uid = 0;
-		}
-
-		if (original_uid != 0) {
-			r = setrlimit(
-			    RLIMIT_MEMLOCK,
-			    &(struct rlimit){MEMLOCK_LIMIT_BYTES, MEMLOCK_LIMIT_BYTES});
-			if (r != 0) {
-#if WARN_ON_MEMORY_LOCK_ERRORS
-				warn("Unable to set RLIMIT_MEMLOCK soft and hard limits to %d "
-				     "bytes, which may cause out of memory errors. Disabling "
-				     "memory locking for future allocations.",
-				     MEMLOCK_LIMIT_BYTES);
-#endif
-				munlockall();
-				mlockall(MCL_CURRENT);
-			}
-			if (setuid(original_uid) != 0) {
-				err(EXIT_OVER_PRIVILEGED, "Unable to drop privileges");
-			}
-		}
-	}
 }
 
 void *malloc_or_exit(size_t n, const char *what) {
